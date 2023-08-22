b71330fce71b24c4fabf67738ab3d4aaf6b787e3
Soe Thura Kyaw <soethurakyaw@ucspathein.edu.mm>
2023-08-23 00:58:10 +0630

    First Commit, Database Created, Purchase Page 50% Done

---
+++ JJStore/Purchase.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/JJStoreMaster.Master" AutoEventWireup="true" CodeFile="Purchase.aspx.cs" Inherits="JJStore.Purchase" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .right-div {
+            position: fixed;
+            top: 0;
+            right: 0;
+            width: 20%;
+            height: 100%;
+            background-color: #f2f2f2;
+            box-shadow: -3px 0px 10px rgba(0, 0, 0, 0.1);
+            z-index: 9999;
+        }
+    </style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script>
+
+    </script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="Purchase" runat="server">
+       <span>Your voucher Code <asp:Label ID="lblVoucherCode" runat="server" ></asp:Label></span>
+           <div style="margin-top: 10px;">
+            <span>Supplier Name</span>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <span>Date</span>
+            <asp:TextBox  ID="TextBox3" runat="server"></asp:TextBox>
+         
+            </div>
+     <div style="float: left">
+            <p>Remark</p>
+            <asp:DropDownList ID="ddlRemark" runat="server"  AutoPostBack="True" OnSelectedIndexChanged="ddlRemark_SelectedIndexChanged">
+                <asp:ListItem Text="new" Value="new"></asp:ListItem>
+                <asp:ListItem Text="none" Value="none"></asp:ListItem>
+                <asp:ListItem Text="change" Value="change"></asp:ListItem>
+             </asp:DropDownList>
+        </div>
+        <div style="float: left;margin-left:20px">
+            <p>Stock ID</p>
+            <asp:TextBox ID="ID" runat="server"></asp:TextBox>
+        </div>
+        <div style="float: left;margin-left:20px">
+            <p>Name</p>
+            <asp:TextBox ID="Name" runat="server"></asp:TextBox>
+        </div>
+        <div style="float: left;margin-left:20px">
+            <p>Qunatity</p>
+            <asp:TextBox ID="Quanty" runat="server"></asp:TextBox>
+              <asp:RegularExpressionValidator ID="NumberValidator" runat="server"
+            ControlToValidate="Quanty"
+            ValidationExpression="^\d+$"
+            ErrorMessage="Only number."
+            Display="Dynamic" />
+        </div>
+        <div style="float: left;margin-left:20px">
+            <p>Price</p>
+            <asp:TextBox ID="Price" runat="server"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server"
+            ControlToValidate="Price"
+            ValidationExpression="^\d+$"
+            ErrorMessage="Only number."
+            Display="Dynamic" />
+        </div>
+        <div style="float: left;margin-left:20px">
+            <p>Total Price</p>
+            <asp:TextBox ID="Total_Price" runat="server"></asp:TextBox>
+             <asp:RegularExpressionValidator ID="RegularExpressionValidator2" runat="server"
+            ControlToValidate="Total_Price"
+            ValidationExpression="^\d+$"
+            ErrorMessage="Only number."
+            Display="Dynamic" />
+        </div>
+      
+   
+        <div style="float: left;margin-left:20px;margin-top:20px">
+            <asp:Button ID="Save" runat="server" Text="Save/Add" OnClick="Save_Click" />
+        </div>
+        <div style="clear:both" ></div>
+
+
+        <h3>Items</h3>
+        <asp:GridView ID="GridView1" runat="server" CssClass="table"
+            width="40%" ShowHeaderWhenEmpty="True" >
+        </asp:GridView>
+
+     <div class="right-div">
+            <h2>Purchase Summary </h2>
+            <div>
+                <span>Voucher Code <asp:Label ID="voucherNo" runat="server"></asp:Label></span> 
+            </div>
+           <div>
+                <span>Supplier Name <asp:Label ID="supplierName" runat="server"></asp:Label></span> 
+            </div>
+           <div>
+                <span>Date <asp:Label ID="purchaseDate" runat="server"></asp:Label></span> 
+            </div>
+           <div>
+                <span>Total Item <asp:Label ID="totalItem" runat="server"></asp:Label></span> 
+            </div>
+           <div>
+                <span>Total Amount <asp:Label ID="totalAmount" runat="server"></asp:Label></span> 
+            </div>
+          <div>
+               <asp:Button ID="saveVoucher" runat="server"></asp:Button>
+            </div>
+        </div>
+   
+</asp:Content>
+
---
+++ JJStore/Purchase.aspx.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JJStore
+{
+    public partial class Purchase : System.Web.UI.Page
+    {
+        string voucherCode;
+        int StockID;
+        string StockName;
+        int Quantity;
+        int PurchasePrice;
+        int TotalPrice;
+        string SupplierName;
+        string Remark;
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ASUS.SOETHURAKYAW\\Desktop\\Computer_Studies_Semester_6\\C#_ADO.Net_ASP.Net\\Practice\\JJStore\\JJStore\\App_Data\\JJStore.mdf;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (!IsPostBack)
+            {
+                if (Session["VoucherCode"] == null)
+                {
+                    voucherCode = GenerateVoucherCodeWithDate(7); // Generate 4 random letters followed by today's date
+                    Console.WriteLine(voucherCode);
+                    Session["VoucherCode"] = voucherCode;
+                 
+                }
+                lblVoucherCode.Text = Session["VoucherCode"].ToString(); // To fix the scenario voucher code
+                
+            }
+        }
+        protected void Save_Click(object sender, EventArgs e)
+        {
+           
+            string remark = ddlRemark.SelectedItem.Text;
+            
+            StockName = Name.Text;
+            PurchasePrice = int.Parse(Price.Text);
+            TotalPrice = int.Parse(Total_Price.Text);
+            Quantity = int.Parse(Quanty.Text);
+            Remark = ddlRemark.SelectedItem.Text;
+
+            if(remark == "new")
+            {
+                int StockID = InsertStockFirstandGetStockID();
+              
+                InsertPurchase(StockID);
+                //insertInventory();
+            }
+            else
+            {
+                StockID = int.Parse(ID.Text);
+                InsertPurchase(StockID);// Automatically update the inventory SQL Trigger
+            }
+            
+        }
+        protected int InsertStockFirstandGetStockID()
+        {
+            int StockID = 0;
+            string query = "INSERT INTO Stock (Name, PurchasePrice) VALUES (@Name, @PurchasePrice)";
+            String query2 = "SELECT Id as lastrow FROM Stock";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters
+                    command.Parameters.AddWithValue("@Name", StockName );
+                    command.Parameters.AddWithValue("@PurchasePrice", PurchasePrice);
+                    connection.Open();
+                    int rowAffected = command.ExecuteNonQuery();
+                    if (rowAffected > 0)
+                    {
+                        using (SqlCommand command2 = new SqlCommand(query2, connection))
+                        {
+                            SqlDataAdapter adapter = new SqlDataAdapter(command2);
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+                            DataRow lastRow = dataTable.Rows[dataTable.Rows.Count - 1];
+                            StockID = (int)lastRow["lastrow"];
+                        }
+                    }
+                }
+            }
+            return StockID;
+        }
+
+        protected void InsertPurchase(int StockID)
+        {
+            string name = GetName(StockID);
+            string query = "INSERT INTO Purchase (VoucherCode, StockID, Name, Quantity, Price, TotalPrice, Remark) VALUES (@VoucherCode, @StockID, @Name, @Quantity, @Price, @TotalPrice, @Remark)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters
+                    command.Parameters.AddWithValue("@VoucherCode", lblVoucherCode.Text);
+                    command.Parameters.AddWithValue("@StockID", StockID);
+                    command.Parameters.AddWithValue("@Name", name );
+                    command.Parameters.AddWithValue("@Quantity", Quantity);
+                    command.Parameters.AddWithValue("@Price", PurchasePrice);
+                    command.Parameters.AddWithValue("@TotalPrice", TotalPrice);
+                    command.Parameters.AddWithValue("@Remark", Remark);
+                    connection.Open();
+                    int rowAffected = command.ExecuteNonQuery();
+                    if(rowAffected > 0)
+                    {
+                        ID.Text = "";
+                        Name.Text = "";
+                        Quanty.Text = "";
+                        Price.Text = "";
+                        Total_Price.Text = "";
+
+                        DataTable data = LoadData();
+                        GridView1.DataSource = data;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+        }
+        private DataTable LoadData()
+        {
+            DataTable data = new DataTable();
+
+            string query = "SELECT * FROM Purchase WHERE VoucherCode = @VoucherCode";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@VoucherCode", lblVoucherCode.Text);
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(data);
+                    }
+                }
+            }
+            return data;
+        }
+
+        protected string GetName(int StockID)
+        {
+            string name = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT * FROM Stock WHERE Id=@Id";
+
+                using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", StockID);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                             name = reader["Name"].ToString();
+                        }
+                    }
+                }
+            }
+            return name;
+        }
+
+
+
+
+
+
+
+        private string GenerateVoucherCodeWithDate(int lettersLength) // Generating random voucher code with totday date
+        {
+            const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var random = new Random();
+
+            string randomLetters = new string(Enumerable.Repeat(letters, lettersLength)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray());
+
+            string formattedDate = DateTime.Now.ToString("yyyyMMdd");
+
+            string voucherCode = randomLetters + formattedDate;
+            return voucherCode;
+        }
+        
+        protected void ddlRemark_SelectedIndexChanged(object sender, EventArgs e) //Handling the event for changing remark
+        {
+            string selectedValue = ddlRemark.SelectedValue;
+            string selectedText =  ddlRemark.SelectedItem.Text;
+
+            if (selectedText == "new")
+            {
+                ID.Enabled = false;
+                Quanty.Enabled = true;
+                Total_Price.Enabled = true;
+                Name.Enabled = true;
+                Price.Enabled = true;
+            }
+            if (selectedText == "none")
+            {
+                Name.Enabled = false;
+                ID.Enabled = true;
+                Quanty.Enabled = true;
+                Total_Price.Enabled = true;
+                Price.Enabled = false;
+            }
+            if (selectedText == "change")
+            {
+                Name.Enabled = false;
+                ID.Enabled = true;
+                Quanty.Enabled = true;
+                Total_Price.Enabled = true;
+                Price.Enabled = true;
+            }
+
+        }
+
+    }
+
+}
